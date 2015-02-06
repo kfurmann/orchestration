14620a5926985b2714c3776cdab01aa1d0a7f516
siata <andrzej.max.marcinkowski@gmail.com>
2015-02-06 19:22:57 +0100

    i

--- Vagrantfile
+++ Vagrantfile
@@ -3,7 +3,12 @@ config.vm.box = "ubuntu/trusty64"
 #config.vm.box_url = "https://vagrantcloud.com/hashicorp/boxes/precise32/versions/1.0.0/providers/virtualbox.box"
 #config.vm.box_url = "https://github.com/kraksoft/vagrant-box-ubuntu/releases/download/14.04/ubuntu-14.04-amd64.box"
 config.vm.provision :shell, path: "bootstrap.sh"
-config.vm.network :forwarded_port, host: 65432, guest: 80
-config.vm.network :forwarded_port, host: 65431, guest: 8080
+config.vm.network :forwarded_port, host: 8000, guest: 80
+config.vm.network :forwarded_port, host: 8080, guest: 8080
+config.vm.network :forwarded_port, host: 4848, guest: 4848
 config.vm.network :forwarded_port, host: 8005, guest: 8005
+config.vm.provider "virtualbox" do |v|
+	v.memory = 2048
+  	v.cpus = 2
+end
 end
--- bootstrap.sh
+++ bootstrap.sh
@@ -1,4 +1,12 @@
 #!/usr/bin/env bash
+PATH=.:$PATH
+ECLIPSE_JEE=eclipse-jee-luna-SR1a-linux-gtk-x86_64.tar.gz
+ECLIPSE_PHP=eclipse-php-luna-SR1a-linux-gtk-x86_64.tar.gz
+SERVER=http://mirror.netcologne.de/eclipse/technology/epp/downloads/release/luna/SR1a/
+#add repos
+add-apt-repository -y ppa:webupd8team/java
+curl -sL https://deb.nodesource.com/setup | sudo bash -
+#update
 apt-get update
 DEBIAN_FRONTEND=noninteractive apt-get -y upgrade
 #deb packages
@@ -15,9 +23,10 @@ apt-get install -y apache2
 a2enmod rewrite
 }
 #mysql
+
 function mysql {
-echo "mysql-server-5.5 mysql-server/root_password password qwe123" | sudo debconf-set-selections
-echo "mysql-server-5.5 mysql-server/root_password_again password qwe123" | sudo debconf-set-selections 
+echo "mysql-server-5.5 mysql-server/root_password password nA8Wedeg" | sudo debconf-set-selections
+echo "mysql-server-5.5 mysql-server/root_password_again password nA8Wedeg" | sudo debconf-set-selections 
 apt-get install -y mysql-server-5.5
 }
 #samba
@@ -32,16 +41,14 @@ apt-get install -y cifs-utils
 function php {
 apt-get install -y php5 php5-curl php5-xdebug 
 echo 'phpmyadmin phpmyadmin/dbconfig-install boolean true' | debconf-set-selections
-echo 'phpmyadmin phpmyadmin/app-password-confirm password zxc123' | debconf-set-selections
-echo 'phpmyadmin phpmyadmin/mysql/admin-pass password qwe123' | debconf-set-selections
-echo 'phpmyadmin phpmyadmin/mysql/app-pass password zxc123' | debconf-set-selections
+echo 'phpmyadmin phpmyadmin/app-password-confirm password nA8Wedeg' | debconf-set-selections
+echo 'phpmyadmin phpmyadmin/mysql/admin-pass password nA8Wedeg' | debconf-set-selections
+echo 'phpmyadmin phpmyadmin/mysql/app-pass password nA8Wedeg' | debconf-set-selections
 echo 'phpmyadmin phpmyadmin/reconfigure-webserver multiselect apache2' | debconf-set-selections
 apt-get install -y phpmyadmin
 }
 #java8
 function java8 {
-add-apt-repository -y ppa:webupd8team/java
-apt-get update
 echo oracle-java8-installer shared/accepted-oracle-license-v1-1 select true | sudo /usr/bin/debconf-set-selections
 apt-get install -y oracle-java8-installer
 apt-get install -y oracle-java8-set-default
@@ -55,30 +62,35 @@ fi
 }
 #nodejs (required by phpdev & javadev)
 function nodejs {
-apt-get purge nodejs npm
-curl -sL https://deb.nodesource.com/setup | sudo bash -
+#apt-get purge nodejs npm
 apt-get install -y nodejs
 apt-get install -y npm
-ln -s /usr/bin/nodejs /usr/bin/node
 npm config set registry http://registry.npmjs.org/
 }
 #phpdev
 function phpdevtools {
 #problem
-sudo curl -sS https://getcomposer.org/installer | php
-sudo mv composer.phar /usr/bin/composer
+curl -sS https://getcomposer.org/installer | php
+#sudo mv composer.phar /usr/bin/composer
 npm install -g bower
 npm install -g grunt-cli
 }
 #javadev
 function javadevtools {
 sudo apt-get install -y maven
+cp /vagrant/settings.xml ~/.m2/
 npm install -g nodeclipse
 }
 #eclipse (export PATH=.:$PATH or eclipse in PATH)
 function eclipsejee {
+cd /vagrant
+if [ ! -f $ECLIPSE_JEE ]; then 
+  wget -q "$SERVER/$ECLIPSE_JEE"; 
+fi
+tar -zxf "/vagrant/$ECLIPSE_JEE" -C /vagrant/eclipsejee
 echo "eclipse plugins"
-# nodeclipse install testng from luna
+cd /vagrant/eclipsejee/eclipse
+nodeclipse install testng from luna
 }
 # docker
 function docker {
@@ -97,5 +109,6 @@ java8
 nodejs
 phpdevtools
 javadevtools
+eclipsejee
 #docker
 vagrant
