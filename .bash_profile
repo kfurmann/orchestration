872e06cc3a66996289072a9cd8d484bfc2d22a36
siata <andrzej.max.marcinkowski@gmail.com>
2015-02-07 18:21:30 +0100

    i

---
+++ .bash_profile
@@ -0,0 +1,5 @@
+echo "loading bash profile";
+export GLASSFISH_PASSWORD=glassfish
+export DATABASE_NAME=hospital
+export DB_USER=hospitaluser
+export DB_PASSWORD=nA8Wedeg
---
+++ db.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+SQL=$(cat /vagrant/db.sql | sed -e "s/DATABASE_NAME/$DATABASE_NAME/" -e "s/DB_USER/$DB_USER/" -e "s/DB_PASSWORD/$DB_PASSWORD/")
+echo $SQL
+echo $SQL | mysql -uroot -p
