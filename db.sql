29935b9f0f2f396643da2862774ff3f677e495cd
amarcinkowski <andrzej.marcinkowski@gmail.com>
2015-04-14 12:38:26 +0200

    db export

--- db.sql
+++ db.sql
@@ -1,5 +1,5 @@
-create database DATABASE_NAME;
-use DATABASE_NAME;
-alter database DATABASE_NAME CHARACTER SET utf8 COLLATE utf8_polish_ci;
+create database DB_NAME;
+use DB_NAME;
+alter database DB_NAME CHARACTER SET utf8 COLLATE utf8_polish_ci;
 create user DB_USER identified by 'DB_PASSWORD';
-grant all on DATABASE_NAME to DB_USER;
+grant all on DB_NAME to DB_USER;
