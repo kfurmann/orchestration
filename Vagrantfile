8966476602a6506aca35bfe3576037577717d228
amarcinkowski <andrzej.marcinkowski@gmail.com>
2015-04-23 12:21:01 +0200

    up

--- Vagrantfile
+++ Vagrantfile
@@ -3,7 +3,7 @@ config.vm.box = #"ubuntu/ubuntu-core-devel-amd64"
 "ubuntu/trusty64"
 #config.vm.box_url = "https://vagrantcloud.com/hashicorp/boxes/precise32/versions/1.0.0/providers/virtualbox.box"
 #config.vm.box_url = "https://github.com/kraksoft/vagrant-box-ubuntu/releases/download/14.04/ubuntu-14.04-amd64.box"
-config.vm.provision :shell, path: "bootstrap.sh"
+config.vm.provision :shell, :path => "bootstrap.sh"
 config.vm.network :forwarded_port, host: 8000, guest: 80
 config.vm.network :forwarded_port, host: 8080, guest: 8080
 #config.vm.network :forwarded_port, host: 4848, guest: 4848
@@ -13,4 +13,5 @@ config.vm.provider "virtualbox" do |v|
 	v.memory = 2048
   	v.cpus = 2
 end
+config.vm.synced_folder "www", "/var/www"
 end
--- bootstrap.sh
+++ bootstrap.sh
@@ -1,19 +1,12 @@
 #!/bin/bash
-echo "mysql-server mysql-server/root_password password $DB_PASSWORD" | sudo debconf-set-selections
-echo "mysql-server mysql-server/root_password_again password $DB_PASSWORD" | sudo debconf-set-selections 
-sudo apt-get install -y apache2 php5 libapache2-mod-php5 mysql-server php5-mysql php5-curl phpunit subversion nodejs git
-sudo service apache2 restart
-curl -sS https://getcomposer.org/installer | sudo php -- --install-dir=/usr/local/bin --filename=composer
+sudo apt-get -y install git
 cd /var/www/
 sudo git clone https://github.com/amarcinkowski/hospitalpage
 mv hospitalpage/* .
 mv hospitalpage/.* .
-composer up -vvv
-cp resources/.env .
+./install-server.sh 
 ./install-dependencies.sh 
 ./install-wp-cli.sh 
-./install-wp-tests.sh wordpress root '' 127.0.0.1 latest
-wp core install --url=127.0.0.1 --title=x --admin_user=root --admin_email=x@x.w --admin_password=x
-wp blog create --url=127.0.0.1
-wp core multisite-convert
-phpunit -c phpunit-wpdb.xml 
+#./install-wp-all.sh 
+#phpunit -c phpunit-wpdb.xml 
+
