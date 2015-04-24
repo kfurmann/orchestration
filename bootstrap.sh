6687dacd6dc305bb93e886fc33b63f793b752885
amarcinkowski <andrzej.marcinkowski@gmail.com>
2015-04-24 09:20:07 +0200

    provisioning done

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,12 +2,11 @@
 DIR=/var/www
 sudo apt-get -y install git
 sudo rm -rf $DIR/* $DIR/.*
-cd $DIR
 sudo -u vagrant git clone https://github.com/amarcinkowski/hospitalpage .
 ./install-server.sh 
-./install-wp-cli.sh 
-echo "installing deps with $1"
-sudo -u vagrant ./install-dependencies.sh $1
+./install-wp-cli.sh
+# su instead of sudo to get HOME dir changed to vagrant user 
+su - vagrant -c "$DIR/install-dependencies.sh $1"
 sudo -u vagrant ./install-wp-all.sh 
-sudo ./install-db.sh
+sudo -u vagrant ./install-db.sh
 #phpunit -c phpunit-wpdb.xml 
