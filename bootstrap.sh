dc7b39edba7e30f74135339f28245d775c9c43a7
amarcinkowski <andrzej.marcinkowski@gmail.com>
2015-04-24 09:59:59 +0200

    i

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-DIR=/var/www
+export APP_DIR=/var/www
+sudo rm -rf $APP_DIR/* $APP_DIR/.*
 sudo apt-get -y install git
-sudo rm -rf $DIR/* $DIR/.*
-sudo -u vagrant git clone https://github.com/amarcinkowski/hospitalpage .
-./install-server.sh 
-./install-wp-cli.sh
+sudo -u vagrant git clone https://github.com/amarcinkowski/hospitalpage $APP_DIR
+sudo $APP_DIR/install-server.sh 
+sudo $APP_DIR/install-wp-cli.sh
 # su instead of sudo to get HOME dir changed to vagrant user 
-su - vagrant -c "$DIR/install-dependencies.sh $1"
-sudo -u vagrant ./install-wp-all.sh 
-sudo -u vagrant ./install-db.sh
+su - vagrant -c "$APP_DIR/install-dependencies.sh $1"
+sudo -u vagrant $APP_DIR/install-wp-all.sh 
+sudo -u vagrant $APP_DIR/install-db.sh
 #phpunit -c phpunit-wpdb.xml 
