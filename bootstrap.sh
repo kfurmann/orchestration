2d6ef84541d7d62cba29fc1c4b9d2e815e9d2440
amarcinkowski <andrzej.marcinkowski@gmail.com>
2015-04-24 08:11:28 +0200

    all scripts

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,10 +3,10 @@ DIR=/var/www
 sudo apt-get -y install git
 sudo rm -rf $DIR/* $DIR/.*
 cd $DIR
-sudo git clone https://github.com/amarcinkowski/hospitalpage .
+sudo -u vagrant git clone https://github.com/amarcinkowski/hospitalpage .
 ./install-server.sh 
-./install-dependencies.sh 
 ./install-wp-cli.sh 
-./install-wp-all.sh 
+sudo -u vagrant ./install-dependencies.sh f9939b8e7846a5bcfbfaea82bc3485ab5531e45d
+sudo -u vagrant ./install-wp-all.sh 
+sudo ./install-db.sh
 #phpunit -c phpunit-wpdb.xml 
-
