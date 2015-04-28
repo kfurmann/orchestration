510ffd4f3ad9a1d799c56de0223dc7d1d638cd17
amarcinkowski <andrzej.marcinkowski@gmail.com>
2015-04-28 14:48:22 +0200

    paths privilieged false

--- Vagrantfile
+++ Vagrantfile
@@ -3,10 +3,11 @@ Vagrant.configure("2") do |config|
   config.vm.box = "ubuntu/trusty64" #"ubuntu/ubuntu-core-devel-amd64" 
   if Vagrant.has_plugin?("vagrant-cachier")
     config.cache.auto_detect = false
+#    config.cache.enable_nfs  = true
     config.cache.enable :apt
     config.cache.enable :composer
   end
-  config.vm.provision :shell, :path => "bootstrap.sh", :args => ENV["GITHUB_OAUTH_TOKEN"]
+  config.vm.provision :shell, :path => "bootstrap.sh", :args => ENV["GITHUB_OAUTH_TOKEN"], privileged: false
   config.vm.network :forwarded_port, host: 8000, guest: 80
   config.vm.network :forwarded_port, host: 8080, guest: 8080
   config.vm.provider "virtualbox" do |v|
--- bootstrap.sh
+++ bootstrap.sh
@@ -4,28 +4,39 @@
 export APP_DIR=/var/www
 export REPO_DIR=/home/vagrant/repo
 github_projects=(hospitalpage hospitalplugin hospitaltheme punction epidemio)
-
+GITHUB_TOKEN=$1
 
 #vagrant prepare
-sudo rm -rf $APP_DIR $REPO_DIR/*
-sudo apt-get -y install git
-for i in "${github_projects[@]}"
-do
-  echo "PROJECT $i"
-  sudo -u vagrant git clone https://github.com/amarcinkowski/$i $REPO_DIR/$i
-done
-sudo rm -rf $APP_DIR
-sudo ln -s $REPO_DIR/hospitalpage $APP_DIR
-
-#server prepare
-sudo $APP_DIR/install-server.sh
+function clone_repos {
+  sudo rm -rf $APP_DIR $REPO_DIR/*
+  sudo apt-get -y install git
+  for i in "${github_projects[@]}"
+  do
+    echo "PROJECT $i"
+    git clone https://github.com/amarcinkowski/$i $REPO_DIR/$i
+  done
+  sudo rm -rf $APP_DIR
+  sudo ln -s $REPO_DIR/hospitalpage $APP_DIR
+}
 
 #wp prepare
-sudo $APP_DIR/install-wp-cli.sh
-su - vagrant -c "/var/www/install-dependencies.sh $1"
-su - vagrant -c "$APP_DIR/install-wp-all.sh"
-su - vagrant -c "$APP_DIR/install-db.sh"
+function wp_prepare {
+  $APP_DIR/install-wp-cli.sh
+  $APP_DIR/install-dependencies.sh $1
+  $APP_DIR/install-wp-all.sh
+  $APP_DIR/install-db.sh
+}
 
 #run tests
-su - vagrant -c "$APP_DIR/install-wp-tests.sh"
-phpunit -c phpunit-wpdb.xml 
+function run_tests {
+  $APP_DIR/install-wp-tests.sh
+  cd $APP_DIR
+  phpunit -c phpunit-wpdb.xml
+}
+
+# ----------------------
+clone_repos
+source $APP_DIR/resources/.env.bash
+sudo $APP_DIR/install-server.sh
+wp_prepare $GITHUB_TOKEN
+run_tests
